7a22da838b433bf1fd4bd37d99052c55a5eb6998
Ryan <tjddnfkdls0@naver.com>
2024-08-19 21:20:41 +0900

    bug fixed

--- src/components/layout/StartLayout.js
+++ src/components/layout/StartLayout.js
@@ -14,15 +14,16 @@ const lineAnimation = keyframes`
 
 const openAnimation = keyframes`
     0%{width : 50%;}
+    20%{width: 50%;}
     100%{width : 0%;}
 `
 const backgroundAnimation = keyframes`
     0%{background-color : black }
-    100%{background-color : transparent;
+    100%{
+        background-color: transparent;
         z-index: -10;
     }
 `
-
 
 const Container = styled.div`
     position: fixed;
@@ -30,7 +31,7 @@ const Container = styled.div`
     z-index : 10000;
     width: 100vw;
     height: 100vh;
-    animation : ${backgroundAnimation} 0.1s ease 2s 1 forwards;
+    animation : ${backgroundAnimation} 0.5s ease 2s 1 forwards;
 `
 
 const LeftBox = styled.div`
@@ -38,14 +39,14 @@ const LeftBox = styled.div`
     background-color: black;
     width: 50%;
     height: 0;
-    animation: ${lineAnimation} 2s ease-in-out 0s 1 forwards, ${openAnimation} 0.5s ease 2.1s 1 forwards;
+    animation: ${lineAnimation} 2s ease-in-out 0s 1 forwards, ${openAnimation} 0.5s ease 2s 1 forwards;
 `
 const RightBox = styled.div`
     border-left : 0.1px solid white;
     background-color: black;
     width: 50%;
     height: 0;
-    animation: ${lineAnimation} 2s ease-in-out 0s 1 forwards, ${openAnimation} 0.5s ease 2.1s 1 forwards;
+    animation: ${lineAnimation} 2s ease-in-out 0s 1 forwards, ${openAnimation} 0.5s ease 2s 1 forwards;
 `
 const Wrap = styled.div`
     width: 100%;
